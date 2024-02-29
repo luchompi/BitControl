23c561a4d1ed840ede6eb88c1635e5845e88f200
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2024-02-29 14:51:06 -0500

    feat(Ver. 0.1): :sparkles: Inventario app (Marcas)
    Se ha creado modelos  y migraciones. Adicional se ha registrado la aplicación en la configuración de django

--- apps/Inventario/models.py
+++ apps/Inventario/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Marca(models.Model):
+    name = models.CharField(max_length=100, unique=True, error_messages={
+        'unique': 'Ya existe una marca con ese nombre'})
+    timestamps = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'whitenoise',
     # LocalApps
     'apps.accounts',
+    'apps.Inventario',
 
 ]
 
