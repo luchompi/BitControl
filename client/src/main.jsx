78945e7460863f887ba1e7568b69b098d1d70e89
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2024-02-29 16:24:32 -0500

    feat(Ver. 0.1): :lipstick: UI
    Se han importado los estilos de iconos y de adminlte a la vista

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "admin-lte/dist/css/adminlte.css";
+import "admin-lte/dist/js/adminlte.min.js";
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
