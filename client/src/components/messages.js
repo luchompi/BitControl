149689479016bde4f0822f5f6ab729e9386e9669
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2024-03-19 16:17:37 -0500

    feat(Ver. 0.1): :sparkles: añadi instancias de sweetalert para usarlas en el proyecto

---
+++ client/src/components/messages.js
@@ -0,0 +1,75 @@
+import Swal from 'sweetalert2'
+
+
+export const successMessage = (title, message) => {
+    Swal.fire({
+        icon: 'success',
+        title: title,
+        text: message,
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        allowEnterKey: false,
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+    })
+}
+
+export const errorMessage = (error) => {
+    const errorArray = error.response.data;
+    let errorMessage = ''
+    try {
+        for (const field in errorArray) {
+            if (errorArray.hasOwnProperty(field)) {
+                errorMessage += `${field}: ${errorArray[field].join(', ')}\n`;
+            }
+        }
+    } catch (error) {
+        errorMessage = errorArray.detail
+    }
+    return Swal.fire({
+        title: 'Error',
+        text: errorMessage,
+        icon: 'error',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        timer: 2000,
+        timerProgressBar: true,
+        showConfirmButton: false
+    })
+}
+
+
+export const confirmMessage = (title, message, confirmButtonText, cancelButtonText, callback, secondaryCallback) => {
+    return Swal.fire({
+        title: title,
+        text: message,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: confirmButtonText,
+        cancelButtonText: cancelButtonText,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false
+    }).then((result) => {
+        if (result.isConfirmed) {
+            callback()
+        }
+        else {
+            secondaryCallback() | null
+        }
+
+    })
+}
+
+export const infoMessage = (title, message) => {
+    return Swal.fire({
+        title: title,
+        text: message,
+        icon: 'info',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false
+    })
+}
+
